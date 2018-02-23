6108ea9512a0c9cd104110ee2a85221d00e265ca
Joanthan Gilyadov <joni.gilyadov03@gmail.com>
2018-02-23 14:09:48 +0300

    Add files via upload

--- css expert/app.js
+++ css expert/app.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const app = express()
 const PORT = 8080
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 
 app.use(express.static('client'))
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("customers", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
 
-app.post("/", function(req, res) {
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("customers");
+  dbo.collection("customers").find({}).toArray(function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    db.close();
+  });
+});
+
+app.post("/signUp", function(req, res) {
   let body = "";
   req.on("data", function(data) {
     body += data
@@ -14,6 +35,42 @@ app.post("/", function(req, res) {
     let jsObject = JSON.parse(body)
     console.log(jsObject.username)
     console.log(typeof(jsObject));
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("customers");
+      dbo.collection("customers").insertOne(jsObject, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+      });
+    });
+
+  })
+})
+
+app.post("/signIn", function(req, res) {
+  let body = "";
+  req.on("data", function(data) {
+    body += data
+  })
+  req.on(`end`, function(){
+    let jsObject = JSON.parse(body)
+    console.log(jsObject.username)
+    console.log(typeof(jsObject));
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("customers");
+      dbo.collection("customers").find({username: jsObject.username, password: jsObject.password}).toArray(function(err, result) {
+        if (err) throw err;
+        if (result.length === 0) {
+          res.send({bool: false})
+        } else {
+          res.send({bool: true})
+        }
+        db.close();
+      });
+    });
+
   })
 })
 
--- css expert/client/js/routes.js
+++ css expert/client/js/routes.js
@@ -1,15 +1,36 @@
 $("#sign-up").submit(function(){
   let newUserDetails = {
-    username: $("#username-input").val() + " " $("#lastName-input").val(),
+    username: $("#username-input").val() + " " + $("#lastName-input").val(),
     email: $("#email-input").val(),
     password: $("#password-input").val(),
     projects: {}
   }
   let json = JSON.stringify(newUserDetails);
   $.ajax({
-    url:"/",
+    url:"/signUp",
     type:"POST",
     dataType: 'json',
     data:json
+  });
+})
+
+$("#sign-in").submit(function() {
+  let usersDetails = {
+    username: $("#usernameLogin").val(),
+    password: $("#passwordLogin").val()
+  }
+  let json = JSON.stringify(usersDetails)
+  $.ajax({
+    url:"/signIn",
+    type:"POST",
+    dataType: 'json',
+    data:json,
+    success: function(data) {
+      if (data.bool) {
+        $("body").html("ACC FOUND!")
+      } else {
+        $("body").html("ACC NOT FOUND!")
+      }
+    }
   });
 })
